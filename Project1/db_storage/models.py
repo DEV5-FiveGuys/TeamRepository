e01e2204073acfe6a46dd73e6af130fe139bee04
gland78 <gland45@naver.com>
2024-12-02 17:25:58 +0900

    [data_desrialize][Chore] add_some_example_files

--- Project1/db_storage/models.py
+++ Project1/db_storage/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    year = models.CharField(max_length=50)
+    score = models.DecimalField(max_digits=4, decimal_places=2)
+    summary = models.TextField()
+    country = models.CharField(max_length=50)
+    genre = models.JSONField()
+    stars = models.JSONField()
+    rank = models.IntegerField()
+
+    def __str__(self):
+        return self.title
