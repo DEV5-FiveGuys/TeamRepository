268d768c9bd985fb2ac2c8a8b8d86bf8e750c1d8
yeoldae <ydy9239@gmail.com>
2024-12-06 00:39:11 +0900

    [data-visualzation][Refactor] 평균별점 import 경로 수정

--- visualizations/visualize_averagerating.py
+++ visualizations/visualize_averagerating.py
@@ -1,5 +1,5 @@
 from statistics import mean
-from IMDb_visualize_TOP5 import display_svg_stars 
+from visualize_TOP5 import display_svg_stars # TOP5 시각화 코드에서 함수 호출
 
 # 평균 별점 계산
 def calculate_average_rating(movies):
@@ -37,59 +37,54 @@ def display_average_rating_in_html(movies):
 # 예시 데이터
 movies = [
     {
-    "img": "https://m.media-amazon.com/images/M/MV5BYTYzMTlmNDctNmVkNS00YzRlLWE5MjAtODdjZWRkYzRlNWVlXkEyXkFqcGc@._V1_QL75_UX72_CR0,0,72,107_.jpg",
+    "rank": 1,
     "title": "오징어 게임",
-    "year": "2021–2025",
-    "score": "8.0",
+    "release_year": "2021–2025",
+    "score": 8.0,
     "summary": "Hundreds of cash-strapped players accept a strange invitation to compete in children's games. Inside, a tempting prize awaits with deadly high stakes: a survival game that has a whopping 45.6 billion-won prize at stake.",
-    "country": "KR",
-    "genre": ["Action", "Drama", "Mystery"],
-    "stars": ["Lee Jung-jae", "Park Hae-soo", "Nandito Hidayattullah Putra"],
-    "rank": 1
+    "image": "https://m.media-amazon.com/images/M/MV5BYTYzMTlmNDctNmVkNS00YzRlLWE5MjAtODdjZWRkYzRlNWVlXkEyXkFqcGc@._V1_QL75_UX72_CR0,0,72,107_.jpg",
+    "genres": ["Action", "Drama", "Mystery"],
+    "actors": ["Lee Jung-jae", "Park Hae-soo", "Nandito Hidayattullah Putra"]
 },
-    {
-    "img": "https://m.media-amazon.com/images/M/MV5BZWMyMjRkYzMtZDMyNS00ZTEwLTg3ZmMtYTljZDMxMjg2MjNhXkEyXkFqcGc@._V1_QL75_UY207_CR3,0,140,207_.jpg",
+{
+    "rank": 2,
     "title": "지금 거신 전화는",
-    "year": "2024-",
-    "score": "8.4",
+    "release_year": "2024-",
+    "score": 8.4,
     "summary": "Baek Sa Eon, a former presidential spokesman, marries Hong Hui Ju, a mute newspaper heiress, in an arranged union. When Hui Ju is kidnapped, their distant relationship is challenged as they navigate the crisis.",
-    "country": "KR",
-    "genre": ["Drama", "Mystery", "Romance", "Thriller"],
-    "stars": ["Yoo Yeon-seok", "Chae Soo-bin", "Heo Nam-jun"],
-    "rank": 2
+    "image": "https://m.media-amazon.com/images/M/MV5BZWMyMjRkYzMtZDMyNS00ZTEwLTg3ZmMtYTljZDMxMjg2MjNhXkEyXkFqcGc@._V1_QL75_UY207_CR3,0,140,207_.jpg",
+    "genres": ["Drama", "Mystery", "Romance", "Thriller"],
+    "actors": ["Yoo Yeon-seok", "Chae Soo-bin", "Heo Nam-jun"]
 },
-    {
-    "img": "https://m.media-amazon.com/images/M/MV5BYjk1Y2U4MjQtY2ZiNS00OWQyLWI3MmYtZWUwNmRjYWRiNWNhXkEyXkFqcGc@._V1_QL75_UX140_CR0,0,140,207_.jpg",
+{
+    "rank": 3,
     "title": "기생충",
-    "year": "2019",
-    "score": "8.5",
+    "release_year": "2019",
+    "score": 8.5,
     "summary": "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
-    "country": "KR",
-    "genre": ["Dark Comedy", "Korean Drama", "Psychological Thriller", "Tragedy", "Drama"],
-    "stars": ["Song Kang-ho", "Cho Yeo-jeong","Lee Sun-kyun", "Choi Woo-sik"],
-    "rank": 3
+    "image": "https://m.media-amazon.com/images/M/MV5BYjk1Y2U4MjQtY2ZiNS00OWQyLWI3MmYtZWUwNmRjYWRiNWNhXkEyXkFqcGc@._V1_QL75_UX140_CR0,0,140,207_.jpg",
+    "genres": ["Dark Comedy", "Korean Drama", "Psychological Thriller", "Tragedy", "Drama"],
+    "actors": ["Song Kang-ho", "Cho Yeo-jeong", "Lee Sun-kyun", "Choi Woo-sik"]
 },
-            {
-    "img": "https://m.media-amazon.com/images/M/MV5BMTI3NTQyMzU5M15BMl5BanBnXkFtZTcwMTM2MjgyMQ@@._V1_QL75_UX140_CR0,0,140,207_.jpg",
+{
+    "rank": 4,
     "title": "올드보이",
-    "year": "2003",
-    "score": "8.3",
+    "release_year": "2003",
+    "score": 8.3,
     "summary": "After being kidnapped and imprisoned for fifteen years, Oh Dae-Su is released, only to find that he must track down his captor in five days.",
-    "country": "KR",
-    "genre": ["Dark Comedy", "One-Person Army Action", "Psychological Thriller", "Action", "Drama"],
-    "stars": ["Choi Min-sik", "Yoo Ji-tae", "Kang Hye-jeong"],
-    "rank": 4
+    "image": "https://m.media-amazon.com/images/M/MV5BMTI3NTQyMzU5M15BMl5BanBnXkFtZTcwMTM2MjgyMQ@@._V1_QL75_UX140_CR0,0,140,207_.jpg",
+    "genres": ["Dark Comedy", "One-Person Army Action", "Psychological Thriller", "Action", "Drama"],
+    "actors": ["Choi Min-sik", "Yoo Ji-tae", "Kang Hye-jeong"]
 },
-                {
-    "img": "https://m.media-amazon.com/images/M/MV5BNjgwYmJhYjgtYzJhYi00NmU5LWE1ZjEtOWRjOWJkN2M3MTU2XkEyXkFqcGc@._V1_QL75_UY207_CR3,0,140,207_.jpg",
+{
+    "rank": 5,
     "title": "강남 비사이드",
-    "year": "2024-",
-    "score": "7.3",
+    "release_year": "2024-",
+    "score": 7.3,
     "summary": "In Gangnam, Seoul, Jae-Hee knows a secret about a series of disappearances but then vanishes herself. Detective Kang, outlaw Yoon, and Prosecutor Min pursue the truth for their own reasons.",
-    "country": "KR",
-    "genre": ["Action", "Crime", "Drama", "Mystery", "Thriller"],
-    "stars": ["Jo Woo-jin", "Ji Chang-wook", "Ha Yoon-kyung"],
-    "rank": 5
+    "image": "https://m.media-amazon.com/images/M/MV5BNjgwYmJhYjgtYzJhYi00NmU5LWE1ZjEtOWRjOWJkN2M3MTU2XkEyXkFqcGc@._V1_QL75_UY207_CR3,0,140,207_.jpg",
+    "genres": ["Action", "Crime", "Drama", "Mystery", "Thriller"],
+    "actors": ["Jo Woo-jin", "Ji Chang-wook", "Ha Yoon-kyung"]
 }
 ]
 
