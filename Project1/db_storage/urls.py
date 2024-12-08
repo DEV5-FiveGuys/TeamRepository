756f3136a2be2c4abd1e518c4fed4ec747f14b7c
gland78 <gland45@naver.com>
2024-12-09 00:14:37 +0900

    [feature/front-end][Fix] 중복 admin 삭제

--- Project1/db_storage/urls.py
+++ Project1/db_storage/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from .views import BulkInsertRankingView, GetMovieListByCountryAPIView, movie_list, movie_details, visualize_country_movies
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('bulk-insert-ranking/', BulkInsertRankingView.as_view(), name='bulk-insert-ranking'),
     path('movies/<str:country_name>/', GetMovieListByCountryAPIView.as_view(), name='movies-by-country'),
     path('', movie_list, name='movie_list'),  # 영화 목록 페이지
