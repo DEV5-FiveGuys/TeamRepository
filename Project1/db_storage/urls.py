706f83ee4d16b8775aa9f1e219008ee4729d5665
5solemi5 <seizeju@gmail.com>
2024-12-08 22:00:08 +0900

    [feature/front-end][Feat]추가 visualize 경로

--- Project1/db_storage/urls.py
+++ Project1/db_storage/urls.py
@@ -1,7 +1,7 @@
 # db_storage/urls.py
 from django.contrib import admin
 from django.urls import path
-from .views import BulkInsertRankingView, GetMovieListByCountryAPIView, movie_list, movie_details
+from .views import BulkInsertRankingView, GetMovieListByCountryAPIView, movie_list, movie_details, visualize_country_movies
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('movies/<str:country_name>/', GetMovieListByCountryAPIView.as_view(), name='movies-by-country'),
     path('', movie_list, name='movie_list'),  # 영화 목록 페이지
     path('movie/<int:movie_id>/', movie_details, name='movie_details'),  # 영화 상세 페이지
+    path('visualize/<str:country_name>/', visualize_country_movies, name='visualize_country_movies'),
 ]
