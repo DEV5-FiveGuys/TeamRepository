c9fc0f8b3ec14aa39b71b951c3cb5279e0bee21b
5solemi5 <seizeju@gmail.com>
2024-12-08 16:27:18 +0900

    Feat: 추가 url

--- Project1/db_storage/urls.py
+++ Project1/db_storage/urls.py
@@ -1,8 +1,12 @@
+# db_storage/urls.py
 from django.contrib import admin
 from django.urls import path
-from .views import BulkInsertRankingView, GetMovieListByCountryAPIView
+from .views import BulkInsertRankingView, GetMovieListByCountryAPIView, movie_list, movie_details
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('bulk-insert-ranking/', BulkInsertRankingView.as_view(), name='bulk-insert-ranking'),
-    path('movies/<str:country_name>/', GetMovieListByCountryAPIView.as_view(), name='movies-by-country')
+    path('movies/<str:country_name>/', GetMovieListByCountryAPIView.as_view(), name='movies-by-country'),
+    path('', movie_list, name='movie_list'),  # 영화 목록 페이지
+    path('movie/<int:movie_id>/', movie_details, name='movie_details'),  # 영화 상세 페이지
 ]
