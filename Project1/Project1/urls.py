2df9a86a2370f34d7fbe5ccb11da88c9222b16a0
gland78 <gland45@naver.com>
2024-12-09 19:01:41 +0900

    [front_test][feature] Root Page 생성

--- Project1/Project1/urls.py
+++ Project1/Project1/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('db_storage.urls')),
+    path('', include('db_storage.urls'))
 ]
